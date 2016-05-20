f951a2d04c379744d98e34735fa89316bfe77c13
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-05-20 09:28:59 -0400

    Implemented shuffle to cards

--- memorygame.rb
+++ memorygame.rb
@@ -3,37 +3,54 @@ def game_over?
   false
 end
 
-def grid
+def reshuffle cards
+  cards.shuffle!
+end
+
+def cards_deck
   cards = [ 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
+  reshuffle cards
+end
+
+def grid
+  cards = cards_deck
   board = ("A".."P").to_a
   grid = board.zip(cards).to_h
-  #binding.pry
 end
 
 def display_grid
   print grid.keys
+  print grid.values
 end
 
 def choose_card
   gets.chomp.upcase
+
 end
 
-def input_validation
-end
+
+
+#correct_guesses
+
+#temporary_guesses
+
 
 def compare_cards grid, card1, card2
+  binding.pry
   grid[card1] == grid[card2]
 
 end
 
 grid
+new_deck = reshuffle cards_deck
 display_grid
+
 
 until game_over?
   first_card = choose_card
   second_card = choose_card
-
   p compare_cards grid, first_card, second_card
+
   #binding.pry
   #record_match
   #replay
